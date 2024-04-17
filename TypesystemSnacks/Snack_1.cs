c26e0318f9dcd9c93fe5ad4fdbedbd6ed8551a98
CalinCretu <calincretu1412@gmail.com>
2024-04-17 14:20:12 +0200

    Aggiungere i file di progetto.

---
+++ TypesystemSnacks/Snack_1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace TypesystemSnacks
+{
+    internal class Snack_1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Snack 1. L’utente inserisce due numeri in successione.\r\nIl software stampa il maggiore.");
+
+            // Chiedo all'utente di inserire il primo numero
+            Console.WriteLine("Inserisci il primo numero");
+
+            // Leggiamo il primo input e lo trasformiamo in un numero intero
+            int primoNumero = int.Parse(Console.ReadLine());
+
+            // Chiedo all'utente di inserire il secondo numero
+            Console.WriteLine("Inserisci il secondo numero");
+
+            // Leggiamo il secondo input e lo trasformiamo in un numero intero
+            int secondoNumero = int.Parse(Console.ReadLine());
+
+            // Verifica quale numero è maggiore tra numero1 e numero2 usando l'operatore ternario
+            int maggiore = (primoNumero > secondoNumero) ? primoNumero : secondoNumero;
+
+            // Stampa il numero maggiore
+            Console.WriteLine("Il numero maggiore è: " + maggiore);
+        }
+    }
+}
