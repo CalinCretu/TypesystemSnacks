4dcf1cbcbc03fb148e27640c8f3599e5f2297a3e
CalinCretu <calincretu1412@gmail.com>
2024-04-17 14:45:08 +0200

    ADD - Snack_2

---
+++ TypesystemSnacks/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace TypesystemSnacks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Snack 1. L’utente inserisce due numeri in successione. Il software stampa il maggiore.
+            Console.WriteLine("Snack 1. L’utente inserisce due numeri in successione. Il software stampa il maggiore.");
+
+            // Chiedo all'utente di inserire il primo numero
+            Console.WriteLine("Inserisci il primo numero!");
+
+            // Leggo il primo input e lo trasformiamo in un numero intero
+            int primoNumero = int.Parse(Console.ReadLine());
+
+            // Chiedo all'utente di inserire il secondo numero
+            Console.WriteLine("Inserisci il secondo numero");
+
+            // Leggo il secondo input e lo trasformiamo in un numero intero
+            int secondoNumero = int.Parse(Console.ReadLine());
+
+            // Verifico quale numero è maggiore tra numero1 e numero2 usando l'operatore ternario
+            int maggiore = (primoNumero > secondoNumero) ? primoNumero : secondoNumero;
+
+            // Stampo il numero maggiore
+            Console.WriteLine("Il numero maggiore è: " + maggiore);
+
+
+            // Snack 2. L’utente inserisce due parole in successione. Il software stampa prima la parola più corta, poi la parola più lunga.
+            Console.WriteLine("Snack 2. L’utente inserisce due parole in successione. Il software stampa prima la parola più corta, poi la parola più lunga.");
+
+            // Chiedo all'utente di inserire la prima parola
+            Console.WriteLine("Inserisci la prima parola!");
+
+            // Leggo il primo input di tipo string e lo assegno alla variabile primaParola
+            string primaParola = Console.ReadLine();
+
+            // Chiedo all'utente di inserire la seconda parola
+            Console.WriteLine("Inserisci la seconda parola!");
+
+            // Leggo il secondo input di tipo string e lo assegno alla variabile secondaParola
+            string secondaParola = Console.ReadLine();
+
+            // Verifico la lunghezza delle parole e le stampo a schermo
+            if (primaParola.Length <= secondaParola.Length)
+            {
+                Console.WriteLine("La parola più corta è: " + primaParola);
+                Console.WriteLine("La parola più lunga è: " + secondaParola);
+            }
+            else
+            {
+                Console.WriteLine("La parola più corta è: " + secondaParola);
+                Console.WriteLine("La parola più lunga è: " + primaParola);
+
+
+            // Snack 3. Il software deve chiedere per 10 volte all’utente di inserire un numero. Il programma stampa la somma di tutti i numeri inseriti.
+            }
+        }
+    }
+}
